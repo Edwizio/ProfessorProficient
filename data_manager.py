7eea60d7a5990253d27a22c01fe254a5fb3ba3b9
Zahra Rauf <zahra.aleem@gmail.com>
2025-10-29 21:25:38 +0100

    create, get and delete methods created for the user

--- data_manager.py
+++ data_manager.py
@@ -1,46 +1,46 @@
 from sqlalchemy.exc import SQLAlchemyError
 from werkzeug.security import generate_password_hash
 
-from data_models import db, Student, Course, Teacher
+from data_models import db, User
 
 
 class DataManager():
     # Define Crud operations as methods
-    def create_student(self, name, password):
-        """This method creates a new student"""
+    def create_user(self, name, password):
+        """This method creates a new user"""
 
         # Hashing the password
         password_hw = generate_password_hash(password)
 
-        new_student = Student(name=name, password=password_hw)
+        new_user = User(name=name, password=password_hw)
 
-        db.session.add(new_student)
+        db.session.add(new_user)
         # Using db.session.rollback() with try-except to avoid crashing if database error occurs
         try:
             db.session.commit()
-            return new_student
+            return new_user
         except SQLAlchemyError:
             db.session.rollback()
             return None
 
 
-    def get_students(self):
-        """This function returns a list of the existing students"""
-        students = Student.query.all()
-        return students
+    def get_users(self):
+        """This function returns a list of the existing users"""
+        users = User.query.all()
+        return users
 
-    def delete_student(self, student_id):
-        """This function deletes a student based on it's ID"""
+    def delete_user(self, student_id):
+        """This function deletes a user based on it's ID"""
 
         # Fetching the correct Student Object
-        student = Student.query.get(student_id)
+        user = User.query.get(student_id)
 
         # Deleting the student from the database
-        db.session.delete(student)
+        db.session.delete(user)
         # Using db.session.rollback() with try-except to avoid crashing if database error occurs
         try:
             db.session.commit()
-            return student
+            return user
         except SQLAlchemyError:
             db.session.rollback()
             return None
