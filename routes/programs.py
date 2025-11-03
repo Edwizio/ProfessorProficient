d688a1cc1081dd8b4d6930225ae36df088a25293
Zahra Rauf <zahra.aleem@gmail.com>
2025-11-03 13:51:30 +0100

    get_programs updated

--- routes/programs.py
+++ routes/programs.py
@@ -2,10 +2,15 @@ from flask import request
 from ProfessorProficient.data_models import Program
 from ProfessorProficient.app import app
 
+
+# Getting a list of all programs
 @app.route("/", methods=["GET"])
 def get_programs():
     """This function returns a list of dictionaries of all programs showing their ID, Name and small description"""
     programs = Program.query.all()
 
-    return [{"id": prog.id, "Name": prog.name ,"Description": prog.description}
+    if not programs:
+        return  {"error": "Sorry no programs have been added to the database"}
+    else:
+        return [{"id": prog.id, "Name": prog.name ,"Description": prog.description}
             for prog in programs], 200
