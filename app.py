3066e70d4bd97207c8cc53712a152f29be93574f
Zahra Rauf <zahra.aleem@gmail.com>
2025-11-05 17:48:14 +0100

    app.py updated and basic backend paths and database table set up in SQLite

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
+import os
+from data_models import db
 
+
+#Creating the flask app object
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-@event.listens_for(Engine, "connect")
-def _fk_on(dbapi_conn, conn_record):
-    cur = dbapi_conn.cursor()
-    cur.execute("PRAGMA foreign_keys=ON;")
-    cur.close()
+
+# Finds the absolute path to our project folder
+basedir = os.path.abspath(os.path.dirname(__file__))
+# The database connection set up which tells that we are using SQLite and base folder has been joined with the subfolder lms.db
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/lms.db')}"
+# Disabling SQLAlchemy’s event system that tracks object changes.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)  # Link the database and the app. This is the reason we need to import db from models
+
+if __name__ == "__main__":
+    """with app.app_context():
+            db.create_all()""" # Creating all the database tables defined by our models.
+    app.run(debug=True)
